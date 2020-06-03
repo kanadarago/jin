c2462be7f269aa08412f0fcc2c5d4c474aabdd0d
hwajung <kanadarago@gmail.com>
2020-06-03 17:59:38 +0900

    js wheel top값 상단에 맞추기

--- b_coding/k_jquery/js/src/mousewheelPage.js
+++ b_coding/k_jquery/js/src/mousewheelPage.js
@@ -4,14 +4,37 @@
 	var win = $(window);
 	var winH = win.innerHeight();
 	$('#wrap').css({height:'auto'});
-	$('.page').css({height:winH});
+	var page = $('.page');
+	page.css({height:winH});
+
 
 	var n = 0;
+	var mvTrue = true;
 
 	win.on('mousewheel DOMMouseScroll',function(e){
 		var evt = e.originalEvent;
 		var result;
-		(evt.wheelDelta)? result = -evt.wheelDelta : result = evt. ;
+		(evt.wheelDelta)? result = -evt.wheelDelta : result = evt.detail * 40;
+		if(mvTrue){
+			mvTrue = false;
+      (result>0) ? n += 1 : n -= 1;
+			//if(result>0){n+=1;}else{n-=1;}
+			if(n >= page.length-1){
+				n == page.length-1;
+			}else if(n < 0){
+				n == 0;
+			}
+			//console.log(n);
+
+			$('html,body').animate({scrollTop: winH * n}, function(){
+				mvTrue = true;
+			});
+
+
+			setTimeout(function(){
+				mvTrue = true;
+			},200)
+		}
 	})
 
 })(jQuery);
