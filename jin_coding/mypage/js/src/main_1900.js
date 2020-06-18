405390046d26118a4f9e2d7e80ff3f5219999478
hwajung <kanadarago@gmail.com>
2020-06-18 18:02:01 +0900

    200617

--- jin_coding/mypage/js/src/main_1900.js
+++ jin_coding/mypage/js/src/main_1900.js
@@ -17,30 +17,37 @@
 //  ----------------------------------------
 
  var slideBox = $('.slide_box');
+ var slideBg = $('.slide_bg');
 
  win.on('scroll',function(e){
 	 var thisTop = $(this).scrollTop();
 	 
-	 if(thisTop < 1041){
-		slideBox.css({'position':'absolute','top':1121+'px','left':251+'px' });
-	 }else if(thisTop < 3060){
-		slideBox.css({'position':'fixed','top':80+'px','left':251+'px'});
+	 if(thisTop < 1100){
+		slideBox.css({'position':'absolute','top':1160+'px','left':251+'px' });
+		// slideBg.css({'marginLeft': 0+'px'});
+	 }else if(thisTop < 3100){
+		slideBox.css({'position':'fixed','top':60+'px','left':251+'px'});
+		// slideBg.stop().animate({'marginLeft': -2289+'px'});
 	 }else{
-		slideBox.css({'position':'absolute','top':3163+'px','left':251+'px' });
+		slideBox.css({'position':'absolute','top':3160+'px','left':251+'px' });
+		// slideBgstop().animate({'marginLeft': -2289+'px'});
 	 }
-
-
-
-
- 	//if(thisTop >= 1041){
- 	//	slideBox.css({'position':'fixed','top':97+'px','left':251+'px'});
- 	//}else if(thisTop <= 2068){
- 	//	slideBox.css({'position':'absolute','top':3163+'px','left':251+'px' });
- 	//}else{
-	//	slideBox.css({'position':'absolute','top':1121+'px','left':251+'px' });
-	// }
  });
 
+// ------------------------------------------------------
+
+win.on('scroll',function(e){
+	var thisTop = $(this).scrollTop();
+	var n=0;
+	if(1100>thisTop){
+		slideBg.stop().animate({'marginLeft': 0+'px'});
+	}else if(thisTop<=3100){
+		var marg = n+thisTop-2001;
+		slideBg.stop().animate({'marginLeft': -marg+'px'});
+	}else if(thisTop<=5844){
+		slideBg.stop().animate({'marginLeft': -1289+'px'});
+	}
+});
 
 
 })(jQuery);
